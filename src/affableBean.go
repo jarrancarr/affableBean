ad21a001b2d140c430dc3f58fd540ace56fc3cfe
jarrancarr <jarran.carr@yahoo.com>
2016-09-23 13:58:07 -0400

    latest

--- src/affableBean.go
+++ src/affableBean.go
@@ -202,7 +202,7 @@ func addPages() {
 }
 
 func chooseLanguage(w http.ResponseWriter, r *http.Request, s *website.Session, p *website.Page) (string, error) {
-		s.Data["language"]=p.Param["language"]
-		http.Redirect(w,r,s.Data["navigation"],302)
-		return "", nil
-	}
+	s.Data["language"]=p.Param["language"]
+	http.Redirect(w,r,s.Data["navigation"],302)
+	return "", nil
+}
